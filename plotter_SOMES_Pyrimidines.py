6bfef5b765bbbb711c5e7f996e820aa770810115
Somes Schwinghammer <sschwingham2671@student.pps.net>
2019-10-31 11:36:19 -0700

    Added Second Half

--- plotter_SOMES_Pyrimidines.py
+++ plotter_SOMES_Pyrimidines.py
@@ -15,10 +15,8 @@ def line(x, y):
   move_to(int(x),int(y))
   pen_up()
 
-def nested_squares_centered1(increase, squares):
+def nested_squares_centered1(increase, squares, x, y):
   pen_up()
-  x = 0
-  y = 0
   move_to(0,-200)
   for num in range(1, squares + 1):
     pen_up()
@@ -29,10 +27,8 @@ def nested_squares_centered1(increase, squares):
     y -= increase//2
     pen_up()
 
-def nested_squares_centered2(increase, squares):
+def nested_squares_centered2(increase, squares, x, y):
   pen_up()
-  x = 232
-  y = 204
   turn_right(18)
   move_to(x/2,y/2)
   turn_right(-36)
@@ -45,10 +41,8 @@ def nested_squares_centered2(increase, squares):
     y -= increase//2
     pen_up()
 
-def nested_squares_centered3(increase, squares):
+def nested_squares_centered3(increase, squares, x, y):
   pen_up()
-  x = 24
-  y = 248
   for num in range(1, squares + 1):
     pen_up()
     move_to(x,y)
@@ -57,18 +51,40 @@ def nested_squares_centered3(increase, squares):
     x -= increase//2
     y -= increase//2
     pen_up()
-    
+
+# BEGIN DRAWING
+
 pen_up()
 turn_right(18)
-nested_squares_centered1(30, 10)
+nested_squares_centered1(30, 10, 90, 0)
 pen_up()
 turn_right(18)
-nested_squares_centered2(25, 10)
+nested_squares_centered2(25, 10, 322, 204)
 pen_up()
-nested_squares_centered3(20, 10)
+nested_squares_centered3(20, 10, 114, 248)
 pen_up()
-move_to(17,11)
-line(250, 207)
-line(32,256)
-line(17,11)
+
+input("Switch to new color, press enter to continue")
+
+move_to(107,11)
+line(340, 207)
+line(122,256)
+line(107,11)
+
+input("Switch to new color, press enter to continue")
+
+pen_up()
+nested_squares_centered1(30, 10, -390, 0)
+turn_right(18)
+nested_squares_centered2(25, 10, -158, 204)
+pen_up()
+nested_squares_centered3(20, 10, -366, 248)
+pen_up()
+
+input("Switch to new color, press enter to continue")
+
+move_to(-373,11)
+line(-140, 207)
+line(-358,256)
+line(-373,11)
 cleanup()
