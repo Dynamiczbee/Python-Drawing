5bf57c455ef11167eea26e19282bf8468d3aae0b
Somes Schwinghammer <sschwingham2671@student.pps.net>
2019-10-31 10:39:19 -0700

    Combined Pyramids and Added Triangle

--- Pyramid in Python.py
+++ Pyramid in Python.py
@@ -10,6 +10,11 @@ def polygon(num_sides, length):
     turn_left(360//num_sides)
   pen_up()
 
+def line(x, y):
+  pen_down()
+  move_to(int(x),int(y))
+  pen_up()
+
 def nested_squares_centered1(increase, squares):
   pen_up()
   x = 0
@@ -17,7 +22,7 @@ def nested_squares_centered1(increase, squares):
   move_to(0,-200)
   for num in range(1, squares + 1):
     pen_up()
-    move_to(x,y)\
+    move_to(x,y)
     pen_down()
     polygon(4, (increase*num))
     x -= increase//2
@@ -26,14 +31,15 @@ def nested_squares_centered1(increase, squares):
 
 def nested_squares_centered2(increase, squares):
   pen_up()
-  x = 200
-  y = 200
+  x = 232
+  y = 204
   turn_right(18)
   move_to(x/2,y/2)
-  pen_down()
   turn_right(-36)
   for num in range(1, squares + 1):
+    pen_up()
     move_to(x,y)
+    pen_down()
     polygon(4, (increase*num))
     x -= increase//2
     y -= increase//2
@@ -41,11 +47,12 @@ def nested_squares_centered2(increase, squares):
 
 def nested_squares_centered3(increase, squares):
   pen_up()
-  x = -200
-  y = 200
+  x = 24
+  y = 248
   for num in range(1, squares + 1):
-    pen_down()
+    pen_up()
     move_to(x,y)
+    pen_down()
     polygon(4, (increase*num))
     x -= increase//2
     y -= increase//2
@@ -53,12 +60,15 @@ def nested_squares_centered3(increase, squares):
     
 pen_up()
 turn_right(18)
-nested_squares_centered1(20, 10)
+nested_squares_centered1(30, 10)
 pen_up()
-move_forward(200)
 turn_right(18)
-nested_squares_centered2(15, 10)
+nested_squares_centered2(25, 10)
 pen_up()
-move_forward(-200)
-nested_squares_centered3(10, 10)
+nested_squares_centered3(20, 10)
+pen_up()
+move_to(17,11)
+line(250, 207)
+line(32,256)
+line(17,11)
 cleanup()
